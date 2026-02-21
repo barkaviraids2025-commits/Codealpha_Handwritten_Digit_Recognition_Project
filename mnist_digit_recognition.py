ce3b02e3f780430519fc3317820e9f5bb8843819
barkaviraids2025-commits <barkavir.aids2025@citchennai.net>
2026-02-21 20:11:52 +0530

    Add files via upload

---
+++ mnist_digit_recognition.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("\nTest Accuracy:", test_acc)
+
+prediction = model.predict(x_test[:1])
+predicted_digit = np.argmax(prediction)
+
+plt.imshow(x_test[0].reshape(28,28), cmap='gray')
+plt.title(f"Predicted Digit: {predicted_digit}")
+plt.show()
+model.save("mnist_model.h5")
